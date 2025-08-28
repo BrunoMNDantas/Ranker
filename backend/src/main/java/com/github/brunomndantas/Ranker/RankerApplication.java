0c4da1549bbd317bc6fcc20134497eff97aa49b7
Bruno Dantas <bmndantas@gmail.com>
2025-08-28 17:44:49 +0100

    Backend inited

---
+++ backend/src/main/java/com/github/brunomndantas/Ranker/RankerApplication.java
@@ -0,0 +1,13 @@
+package com.github.brunomndantas.Ranker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/brunomndantas/Ranker/RankerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.brunomndantas.Ranker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
