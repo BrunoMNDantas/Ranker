a7310e39843a1934ac72f7f9266b26e57f4b0a82
Bruno Dantas <bmndantas@gmail.com>
2025-09-01 18:47:39 +0100

    Routes fixed on App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import classes from './App.module.css';
 import { Route, Routes, useNavigate } from 'react-router-dom';
-import RankPage from './ui/pages/rank/RankPage';
-import TierPage from './ui/pages/tier/TierPage';
-import OptionPage from './ui/pages/option/OptionPage';
-import VotePage from './ui/pages/vote/VotePage';
-import RanksPage from './ui/pages/ranks/RanksPage';
-import TiersPage from './ui/pages/tiers/TiersPage';
-import OptionsPage from './ui/pages/options/OptionsPage';
-import VotesPage from './ui/pages/votes/VotesPage';
+import RankPage from './ui/pages/management/entity/rankPage/RankPage';
+import TierPage from './ui/pages/management/entity/tierPage/TierPage';
+import OptionPage from './ui/pages/management/entity/optionPage/OptionPage';
+import VotePage from './ui/pages/management/entity/votePage/VotePage';
+import RanksPage from './ui/pages/management/entities/ranksPage/RanksPage';
+import TiersPage from './ui/pages/management/entities/tiersPage/TiersPage';
+import OptionsPage from './ui/pages/management/entities/optionsPage/OptionsPage';
+import VotesPage from './ui/pages/management/entities/votesPage/VotesPage';
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import AssignmentsPage from './ui/pages/management/entities/assignmentsPage/AssignmentsPage';
+import AssignmentPage from './ui/pages/management/entity/assignmentPage/AssignmentPage';
 
 
 const App = () => {
@@ -22,22 +24,25 @@ const App = () => {
 
             <div className={classes.content}>
                 <Routes>
-                    <Route path="/rank" element={<RanksPage/>} />
-                    <Route path="/rank/:rankId" element={<RankPage/>} />
-                    <Route path="/tier" element={<TiersPage/>} />
-                    <Route path="/tier/:tierId" element={<TierPage/>} />
-                    <Route path="/option" element={<OptionsPage/>} />
-                    <Route path="/option/:optionId" element={<OptionPage/>} />
-                    <Route path="/vote" element={<VotesPage/>} />
-                    <Route path="/vote/:voteId" element={<VotePage/>} />
+                    <Route path="/management/rank" element={<RanksPage/>} />
+                    <Route path="/management/rank/:rankId" element={<RankPage/>} />
+                    <Route path="/management/tier" element={<TiersPage/>} />
+                    <Route path="/management/tier/:tierId" element={<TierPage/>} />
+                    <Route path="/management/option" element={<OptionsPage/>} />
+                    <Route path="/management/option/:optionId" element={<OptionPage/>} />
+                    <Route path="/management/vote" element={<VotesPage/>} />
+                    <Route path="/management/vote/:voteId" element={<VotePage/>} />
+                    <Route path="/management/assignment" element={<AssignmentsPage/>} />
+                    <Route path="/management/assignment/:assignmentId" element={<AssignmentPage/>} />
                 </Routes>
             </div>
 
              <BottomNavigation showLabels>
-                <BottomNavigationAction label="Ranks" onClick={() => navigate("Rank")}/>
-                <BottomNavigationAction label="Tiers" onClick={() => navigate("Tier")}/>
-                <BottomNavigationAction label="Options" onClick={() => navigate("Option")}/>
-                <BottomNavigationAction label="Votes" onClick={() => navigate("Vote")}/>
+                <BottomNavigationAction label="Ranks" onClick={() => navigate("management/rank")}/>
+                <BottomNavigationAction label="Tiers" onClick={() => navigate("management/tier")}/>
+                <BottomNavigationAction label="Options" onClick={() => navigate("management/option")}/>
+                <BottomNavigationAction label="Votes" onClick={() => navigate("management/vote")}/>
+                <BottomNavigationAction label="Assignments" onClick={() => navigate("management/assignment")}/>
             </BottomNavigation>
         </div>
     );
