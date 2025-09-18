3594774d7d01572683a3aebf8e78a1cf6249b6d8
Bruno Dantas <bmndantas@gmail.com>
2025-09-18 23:22:04 +0100

    Management pages removed

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './App.module.css';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import AppLayout from '../layouts/appLayout/AppLayout';
 import AppRankPage from '../pages/rankPage/RankPage';
 import AppRanksPage from '../pages/ranksPage/RanksPage';
@@ -8,29 +8,18 @@ import AppOptionPage from '../pages/optionPage/OptionPage';
 import AppTierPage from '../pages/tierPage/TierPage';
 import AppVotePage from '../pages/votePage/VotePage';
 import AppAssignmentPage from '../pages/assignmentPage/AssignmentPage';
-import ManagementLayout from '../layouts/managementLayout/ManagementLayout';
-import ManagementRanksPage from '../pages/management/entities/ranksPage/RanksPage';
-import ManagementRankPage from '../pages/management/entity/rankPage/RankPage';
-import ManagementTiersPage from '../pages/management/entities/tiersPage/TiersPage';
-import ManagementTierPage from '../pages/management/entity/tierPage/TierPage';
-import ManagementOptionsPage from '../pages/management/entities/optionsPage/OptionsPage';
-import ManagementOptionPage from '../pages/management/entity/optionPage/OptionPage'
-import ManagementVotesPage from '../pages/management/entities/votesPage/VotesPage';
-import ManagementVotePage from '../pages/management/entity/votePage/VotePage';
-import ManagementAssignmentsPage from '../pages/management/entities/assignmentsPage/AssignmentsPage';
-import ManagementAssignmentPage from '../pages/management/entity/assignmentPage/AssignmentPage';
-import EntryPage from '../pages/entryPage/EntryPage';
+
 import {
-    ROOT_ROUTE, APP_ROUTE, MANAGEMENT_ROUTE,
-    RANK_SUB_ROUTE, TIER_SUB_ROUTE, OPTION_SUB_ROUTE, VOTE_SUB_ROUTE, ASSIGNMENT_SUB_ROUTE,
-    ASSIGNMENTS_SUB_ROUTE, VOTES_SUB_ROUTE, OPTIONS_SUB_ROUTE, TIERS_SUB_ROUTE, RANKS_SUB_ROUTE
+    ROOT_ROUTE, APP_ROUTE, RANK_SUB_ROUTE, TIER_SUB_ROUTE,
+    OPTION_SUB_ROUTE, VOTE_SUB_ROUTE, ASSIGNMENT_SUB_ROUTE,
+    RANKS_SUB_ROUTE, APP_RANKS_ROUTE
 } from './Routes';
 
 const App = () => {
     return (
         <div className={classes.root}>
             <Routes>
-                <Route path={ROOT_ROUTE} element={<EntryPage/>} />
+                <Route path={ROOT_ROUTE} element={<Navigate to={APP_RANKS_ROUTE} replace/>}/>
 
                 <Route path= {APP_ROUTE} element={<AppLayout/>}>
                     <Route index element={<AppRanksPage/>} />
@@ -40,20 +29,6 @@ const App = () => {
                     <Route path={TIER_SUB_ROUTE} element={<AppTierPage/>} />
                     <Route path={VOTE_SUB_ROUTE} element={<AppVotePage/>} />
                     <Route path={ASSIGNMENT_SUB_ROUTE} element={<AppAssignmentPage/>} />
-                </Route>
-
-                <Route path= {MANAGEMENT_ROUTE} element={<ManagementLayout/>}>
-                    <Route index element={<ManagementRanksPage/>} />
-                    <Route path={RANKS_SUB_ROUTE} element={<ManagementRanksPage/>} />
-                    <Route path={RANK_SUB_ROUTE} element={<ManagementRankPage/>} />
-                    <Route path={TIERS_SUB_ROUTE} element={<ManagementTiersPage/>} />
-                    <Route path={TIER_SUB_ROUTE} element={<ManagementTierPage/>} />
-                    <Route path={OPTIONS_SUB_ROUTE} element={<ManagementOptionsPage/>} />
-                    <Route path={OPTION_SUB_ROUTE} element={<ManagementOptionPage/>} />
-                    <Route path={VOTES_SUB_ROUTE} element={<ManagementVotesPage/>} />
-                    <Route path={VOTE_SUB_ROUTE} element={<ManagementVotePage/>} />
-                    <Route path={ASSIGNMENTS_SUB_ROUTE} element={<ManagementAssignmentsPage/>} />
-                    <Route path={ASSIGNMENT_SUB_ROUTE} element={<ManagementAssignmentPage/>} />
                 </Route>
             </Routes>
         </div>
--- frontend/src/app/Routes.ts
+++ frontend/src/app/Routes.ts
@@ -2,8 +2,6 @@ export const ROOT_ROUTE = "/"
 
 export const APP_ROUTE = "/app"
 
-export const MANAGEMENT_ROUTE = "/management"
-
 export const RANK_ID_PLACEHOLDER = ":rankId"
 export const TIER_ID_PLACEHOLDER = ":tierId"
 export const OPTION_ID_PLACEHOLDER = ":optionId"
@@ -25,41 +23,20 @@ export const ASSIGNMENT_SUB_ROUTE = ASSIGNMENTS_SUB_ROUTE + "/" + ASSIGNMENT_ID_
 export const APP_RANKS_ROUTE = APP_ROUTE + "/" + RANKS_SUB_ROUTE
 export const APP_RANK_ROUTE = APP_RANKS_ROUTE + "/" + RANK_ID_PLACEHOLDER
 
-export const MANAGEMENT_RANKS_ROUTE = MANAGEMENT_ROUTE + "/" + RANKS_SUB_ROUTE
-export const MANAGEMENT_RANK_ROUTE = MANAGEMENT_RANKS_ROUTE + "/" + RANK_ID_PLACEHOLDER
-
 export const APP_TIERS_ROUTE = APP_ROUTE + "/" + TIERS_SUB_ROUTE
 export const APP_TIER_ROUTE = APP_TIERS_ROUTE + "/" + TIER_ID_PLACEHOLDER
-
-export const MANAGEMENT_TIERS_ROUTE = MANAGEMENT_ROUTE + "/" + TIERS_SUB_ROUTE
-export const MANAGEMENT_TIER_ROUTE = MANAGEMENT_TIERS_ROUTE + "/" + TIER_ID_PLACEHOLDER
 
 export const APP_OPTIONS_ROUTE = APP_ROUTE + "/" + OPTIONS_SUB_ROUTE
 export const APP_OPTION_ROUTE = APP_OPTIONS_ROUTE + "/" + OPTION_ID_PLACEHOLDER
 
-export const MANAGEMENT_OPTIONS_ROUTE = MANAGEMENT_ROUTE + "/" + OPTIONS_SUB_ROUTE
-export const MANAGEMENT_OPTION_ROUTE = MANAGEMENT_OPTIONS_ROUTE + "/" + OPTION_ID_PLACEHOLDER
-
 export const APP_VOTES_ROUTE = APP_ROUTE + "/" + VOTES_SUB_ROUTE
 export const APP_VOTE_ROUTE = APP_VOTES_ROUTE + "/" + VOTE_ID_PLACEHOLDER
 
-export const MANAGEMENT_VOTES_ROUTE = MANAGEMENT_ROUTE + "/" + VOTES_SUB_ROUTE
-export const MANAGEMENT_VOTE_ROUTE = MANAGEMENT_VOTES_ROUTE + "/" + VOTE_ID_PLACEHOLDER
-
 export const APP_ASSIGNMENTS_ROUTE = APP_ROUTE + "/" + ASSIGNMENTS_SUB_ROUTE
 export const APP_ASSIGNMENT_ROUTE = APP_ASSIGNMENTS_ROUTE + "/" + ASSIGNMENT_ID_PLACEHOLDER
-
-export const MANAGEMENT_ASSIGNMENTS_ROUTE = MANAGEMENT_ROUTE + "/" + ASSIGNMENTS_SUB_ROUTE
-export const MANAGEMENT_ASSIGNMENT_ROUTE = MANAGEMENT_ASSIGNMENTS_ROUTE + "/" + ASSIGNMENT_ID_PLACEHOLDER
 
 export const appRankRoute = (rankId: string) => APP_RANK_ROUTE.replace(RANK_ID_PLACEHOLDER, rankId)
 export const appTierRoute = (tierId: string) => APP_TIER_ROUTE.replace(TIER_ID_PLACEHOLDER, tierId)
 export const appOptionRoute = (optionId: string) => APP_OPTION_ROUTE.replace(OPTION_ID_PLACEHOLDER, optionId)
 export const appVoteRoute = (voteId: string) => APP_VOTE_ROUTE.replace(VOTE_ID_PLACEHOLDER, voteId)
 export const appAssignmentRoute = (assignmentId: string) => APP_ASSIGNMENT_ROUTE.replace(ASSIGNMENT_ID_PLACEHOLDER, assignmentId)
-
-export const managementRankRoute = (rankId: string) => MANAGEMENT_RANK_ROUTE.replace(RANK_ID_PLACEHOLDER, rankId)
-export const managementTierRoute = (tierId: string) => MANAGEMENT_TIER_ROUTE.replace(TIER_ID_PLACEHOLDER, tierId)
-export const managementOptionRoute = (optionId: string) => MANAGEMENT_OPTION_ROUTE.replace(OPTION_ID_PLACEHOLDER, optionId)
-export const managementVoteRoute = (voteId: string) => MANAGEMENT_VOTE_ROUTE.replace(VOTE_ID_PLACEHOLDER, voteId)
-export const managementAssignmentRoute = (assignmentId: string) => MANAGEMENT_ASSIGNMENT_ROUTE.replace(ASSIGNMENT_ID_PLACEHOLDER, assignmentId)
