dbd7801d35734f085943e119e6031250443e46d8
Bruno Dantas <bmndantas@gmail.com>
2025-09-30 15:25:29 +0100

    AuthStore implemented to set ownerId property

---
+++ frontend/src/services/store/Auth.store.ts
@@ -0,0 +1,40 @@
+import Store, { Entity as StoreEntity } from "./Store";
+
+export interface Entity extends StoreEntity {
+    ownerId: string
+}
+
+export default class AuthStore<T extends Entity> implements Store<T> {
+
+    sourceStore: Store<T>
+    ownerIdGetter: (entity: T) => string
+
+
+    constructor(sourceStore: Store<T>, ownerIdGetter: (entity: T)=>string) {
+        this.sourceStore = sourceStore
+        this.ownerIdGetter = ownerIdGetter
+    }
+
+
+    getAll(): Promise<T[]> {
+        return this.sourceStore.getAll()
+    }
+
+    get(id: string): Promise<T|null> {
+        return this.sourceStore.get(id)
+    }
+
+    create(entity: T): Promise<string> {
+        entity.ownerId = this.ownerIdGetter(entity)
+        return this.sourceStore.create(entity)
+    }
+
+    update(entity: T): Promise<void> {
+        return this.sourceStore.update(entity)
+    }
+
+    delete(id: string): Promise<void> {
+        return this.sourceStore.delete(id)
+    }
+
+}
