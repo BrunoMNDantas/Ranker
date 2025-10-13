c4b8046e3a0d0e3fbae50e332a4c05f3eccff4ed
Bruno Dantas <bmndantas@gmail.com>
2025-10-13 20:51:10 +0100

    Removed all import of "React"

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import classes from './App.module.css';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import AppLayout from '../layouts/appLayout/AppLayout';
